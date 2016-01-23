f6615c2c1f42328176fa0c749bcc2abbfa2dc83f
chexingyou <chexingyou@anhouse.com>
2016-01-23 11:52:10 +0800

    update

--- libs/generate.js
+++ libs/generate.js
@@ -2,6 +2,8 @@ var ejs = require('ejs');
 var fs = require('fs');
 var marked = require('marked');
 
+var baseUrl = 'https://hingsir.github.io/jstips-site/';
+
 marked.setOptions({
   renderer: new marked.Renderer(),
   gfm: true,
@@ -21,13 +23,17 @@ marked.setOptions({
 var generate = {
     generate: function(tips){
         tips.forEach(function(tip,index){
+            var url = 'dist/tips/'+ tip.id +'.html';
             ejs.renderFile('views/layout.html',{
                 tips: tips,
+                tip: tip,
                 current: index,
-                content: marked(tip.source.title + tip.source.content)
+                content: marked(tip.source.title + tip.source.content),
+                url: baseUrl + url,
+                shareImg: baseUrl + 'dist/images/github.jpg'
             }, function(err,result){
                 if(err) throw err;
-                fs.writeFile('dist/tips/'+ tip.id +'.html',result,'utf-8');
+                fs.writeFile(url,result,'utf-8');
                 if(index == 0){
                     fs.writeFile('dist/tips/index.html',result,'utf-8');
                 }
