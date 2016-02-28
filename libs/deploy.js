e52fb1049637ca41f150e3b1bebdd6161ca214fd
hingsir <hingsir1024@gmail.com>
2016-02-28 20:00:28 +0800

    update site at Sun Feb 28 2016 20:00:28 GMT+0800 (CST)

--- libs/deploy.js
+++ libs/deploy.js
@@ -1,6 +1,7 @@
 var exec = require('child_process').exec;
 
 exec('git add -A', function(err, stdout, stderr){
+	console.log(stdout)
 	exec('git commit -m "update site at ' + new Date + '"' ,function(err, stdout, stderr){
 		exec('git push origin gh-pages',function(err, stdout, stderr){
 		})
