3a8f29e9871e0ac7225b23da7bad8aa3605af69c
hingsir <hingsir1024@gmail.com>
2016-03-10 20:21:32 +0800

    Update site at Thu Mar 10 2016 20:21:32 GMT+0800 (CST)

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	baseUrl: 'http://jstips.hingsir.com/',
 	seo: {
-		title: 'jstips - A JS tip per day',
-		keywords:'jstips,tips,js,javascript,front end',
+		title: 'JsTips - Useful JavaScript Tips',
+		keywords:'jstips,tips,Useful JavaScript Tips',
 		description: 'Introduces these short and useful daily JavaScript tips that will allow you to improve your code writing. With less than 2 minutes each day, you will be able to read about performance, conventions, hacks, interview questions and all the items that the future of this awesome language holds for us'
 	},
 	highlight_style: 'github',
